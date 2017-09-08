8d69c706ed0196dab9f08b4a41fa0bdd89499054
mmathew <mmathew@SUYPC194>
2017-09-08 17:16:31 +0530

    Add project files.

---
+++ Caching/CachingDemo.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CachingDemo.aspx.cs" Inherits="Caching.CachingDemo" %>
+<%@ OutputCache Duration="60" VaryByParam="DropDownList1" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownList1" runat="server" Width="85px" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="true">
+            <asp:ListItem Text="All" Value="All" Selected="True"></asp:ListItem> 
+            <asp:ListItem Text="Developer" Value="Developer"></asp:ListItem> 
+            <asp:ListItem Text="Tester" Value="Tester"></asp:ListItem> 
+            <asp:ListItem Text="Hr" Value="Hr"></asp:ListItem> 
+        </asp:DropDownList>
+    
+        <asp:GridView ID="GridView1" runat="server">
+          
+        </asp:GridView>
+   
+    Server Time: <asp:Label ID="Label1" runat="server"></asp:Label>
+        <br />
+        Client Time:<script type="text/javascript"> document.write(Date()); </script>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Caching/CachingDemo.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Caching
+{
+    public partial class CachingDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Cache.SetExpires(DateTime.Now.AddSeconds(60));
+            Response.Cache.VaryByParams["None"] = true;
+            Response.Cache.SetCacheability(HttpCacheability.Server);
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da = new SqlDataAdapter("spGetEmployee", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataSet DS = new DataSet();
+            da.Fill(DS);
+            GridView1.DataSource = DS;
+            GridView1.DataBind();
+            Label1.Text = DateTime.Now.ToString();
+            if (!IsPostBack)
+            {
+                GetEmployeeByDesignation("All");
+            }
+
+        }
+        private void GetEmployeeByDesignation(string v)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da = new SqlDataAdapter("spGetEmployeeByDesignation", con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter paramEmployeeDesignation = new SqlParameter();
+            paramEmployeeDesignation.ParameterName = "@EmployeeDesignation";
+            paramEmployeeDesignation.Value = v;
+            da.SelectCommand.Parameters.Add(paramEmployeeDesignation);
+
+            DataSet DS = new DataSet();
+            da.Fill(DS);
+            GridView1.DataSource = DS;
+            GridView1.DataBind();
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GetEmployeeByDesignation(DropDownList1.SelectedItem.Value);
+        }
+    }
+}
